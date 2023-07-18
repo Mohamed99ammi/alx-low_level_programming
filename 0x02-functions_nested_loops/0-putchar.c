70b2afbb369e639c4aeca4d7ce0b0261377452d8
mohamed ammi <mohamedammi401@gmail.com>
2023-07-18 08:08:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
-
+#include "main.h"
+/**
+ * main - entry point
+ * Description Prints the alphabet in lowercase and uppercase
+ */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
